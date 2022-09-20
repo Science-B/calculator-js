a6e4c504fa45cfc49d28edd06a03b66e9e0e13ea
Dmitriy <dmitriytwd@gmail.com>
2022-09-20 18:01:01 +0300

    v.1.0.3

--- index.js
+++ index.js
@@ -6,14 +6,16 @@ let haveDotX = false;
 let haveDotY = false;
 
 const digits = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "."];
-const operations = ["÷", "x", "-", "+", "AC"];
+const operations = ["÷", "x", "-", "+"];
 
 const result = document.querySelector(".calc-result");
 
 const buttons = document.querySelector(".calc");
+
 buttons.addEventListener("click", (event) => {
   if (event.target.classList.contains("btn")) {
     const btn = event.target.textContent;
+
     if (digits.includes(btn)) {
       if (y === "" && action === "") {
         if (x.length <= 15) {
@@ -22,6 +24,7 @@ buttons.addEventListener("click", (event) => {
           } else if (btn === "." && haveDotX === false) {
             x += btn;
             haveDotX = true;
+            console.log("length", x.length);
           }
         }
         result.textContent = x;
@@ -35,19 +38,64 @@ buttons.addEventListener("click", (event) => {
             y += btn;
           } else if (btn === "." && haveDotY === false) {
             y += btn;
-            haveDotY = true;
+            haveDotX = true;
           }
         }
         result.textContent = y;
       }
     }
+
     if (operations.includes(btn)) {
-      if (btn === "AC") {
-        clearAll();
-        return;
-      }
       action = btn;
       result.textContent = action;
+    }
+
+    if (btn === "AC") {
+      clearAll();
+      return;
+    }
+
+    if (btn === "+/-") {
+      if (x !== "" && y === "") {
+        x = -x;
+        result.textContent = x;
+      }
+      if (x !== "" && y !== "") {
+        y = -y;
+        result.textContent = y;
+      }
+      if (finish === true) {
+        x = -x;
+        result.textContent = x;
+      }
+    }
+
+    if (btn === "С") {
+      if (!finish) {
+        if (x !== "" && action === "" && y === "") {
+          if (result.textContent.length > 1) {
+            x = result.textContent.substring(0, result.textContent.length - 1);
+            result.textContent = x;
+            return;
+          } else {
+            clearAll();
+          }
+        }
+        if (x !== "" && action !== "" && y === "") {
+          action = "";
+          result.textContent = x;
+        }
+        if (x !== "" && action !== "" && y !== "") {
+          if (result.textContent.length > 1) {
+            y = result.textContent.substring(0, result.textContent.length - 1);
+            result.textContent = y;
+            return;
+          } else {
+            y = "";
+            result.textContent = action;
+          }
+        }
+      }
     }
 
     if (btn === "=") {
@@ -60,15 +108,19 @@ buttons.addEventListener("click", (event) => {
       }
       haveDotX = false;
       haveDotY = false;
+
       switch (action) {
         case "+":
           x = +x + +y;
+          finish = false;
           break;
         case "-":
           x = x - y;
+          finish = false;
           break;
         case "x":
           x = x * y;
+          finish = false;
           break;
         case "÷":
           if (y === "0") {
@@ -79,6 +131,7 @@ buttons.addEventListener("click", (event) => {
             return;
           }
           x = x / y;
+          finish = false;
           break;
       }
       finish = true;
